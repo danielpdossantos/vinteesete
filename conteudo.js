95766145aa2fc75e77f6d6e41381f4bb580770c4
danielpdossantos <164639267+danielpdossantos@users.noreply.github.com>
2026-03-27 00:54:05 -0300

    Update conteudo.js

--- conteudo.js
+++ conteudo.js
@@ -3,12 +3,10 @@
   const btnNao = document.getElementById('nao');
   const mensagemSim = document.getElementById('mensagemSim');
 
-  // Botão "Sim"
   btnSim.addEventListener('click', () => {
     mensagemSim.style.display = 'block';
   });
 
-  // Botão "Não"
   btnNao.addEventListener('mouseover', () => {
     const x = Math.random() * (window.innerWidth - btnNao.offsetWidth - 40);
     const y = Math.random() * (window.innerHeight - btnNao.offsetHeight - 40);
